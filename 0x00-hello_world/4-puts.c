5506384507094cd4ccda8dd1abd1bbea1fc6faff
Abdalrahman-H <abdalrahmansph003@gmail.com>
2023-03-16 17:24:18 -0700

    MY 4 commit in project c language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A program Writen C language
+ * 
+ * Return: Zero is (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
