472285e64154c7a348f0928c13a0e47b303fb7fe
Abdalrahman-H <abdalrahmansph003@gmail.com>
2023-03-17 18:25:54 -0700

    I hate the Betty :(:( and I need sleeeeeeeeeeeeeeeeep, please

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Desciption: this programm for pring random value and display result
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (i = 97; x < 123; x++)
+	{
+		if (x != 101 && x != 113)
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
