afc413a03553676a09b6f02616eed019d09b0b10
Abdalrahman-H <abdalrahmansph003@gmail.com>
2023-03-23 19:35:31 -0700

    C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * more_numbers - print 0 - 14 ten times and you
- * 			can ouly use _putchar three times
+ *			can ouly use _putchar three times
  *
  * Return: Always 0 (Success)
- * */
+ */
 
 void more_numbers(void)
 {
 	int i, j;
-	
-	for (i = 0; i < 10; i++) 
+
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
