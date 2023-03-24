f40f45157f40b6c143ed150be2aa1afbd4062fc0
Abdalrahman-H <abdalrahmansph003@gmail.com>
2023-03-24 08:08:53 -0700

    D

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_numbers - print 0 - 9
  *		only using _putchar twice
+ * Return: the number since 0 up to 9
  */
 
 void print_numbers(void)
@@ -11,7 +12,7 @@ void print_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar((n % 10) + '0');
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
